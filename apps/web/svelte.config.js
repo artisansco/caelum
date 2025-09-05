ec3ade66616c2a104b13a1f78d4531f7ac60f922
Sparrow <benjaminthorpe19@gmail.com>
2025-09-05 07:28:28 +0000

    fix: enable melt-ui preprocessor

--- apps/web/svelte.config.js
+++ apps/web/svelte.config.js
@@ -1,15 +1,15 @@
+import { preprocessMeltUI, sequence } from "@melt-ui/pp";
 import adapter from "@sveltejs/adapter-auto";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
-/** @type {import('@sveltejs/kit').Config} */
+/** @type {import('@sveltejs/kit').Config}*/
 const config = {
-	preprocess: vitePreprocess(),
+	preprocess: sequence([vitePreprocess(), preprocessMeltUI()]),
 	compilerOptions: {
 		experimental: {
 			async: true,
 		},
 	},
-
 	kit: {
 		adapter: adapter(),
 		experimental: {
@@ -17,5 +17,4 @@ const config = {
 		},
 	},
 };
-
 export default config;
