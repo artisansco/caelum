3182e76d2a0fa169f9df043ee46f903856a57cde
Sparrow <benjaminthorpe19@gmail.com>
2025-10-07 06:39:57 +0000

    chore: clean last remaining api codebase

--- api/index.ts
+++
@@ -1,7 +0,0 @@
-import { serve } from "@hono/node-server";
-import app from "./src/app";
-import { config } from "./src/lib/config";
-
-serve({ fetch: app.fetch, port: config.PORT }, (info) => {
-	console.log(`Server is running on http://localhost:${info.port}`);
-});
